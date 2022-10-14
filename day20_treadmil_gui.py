7a190acd53299f5db424c9166a60fd610493e721
prerna <preranachauhan742@gmail.com>
2022-10-14 15:41:45 +0530

    day 20 treadmil_gui.py

---
+++ day20_treadmil_gui.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tkinter as ttk
+
+data = pd.read_csv('treadmil-users.csv')
+
+app = ttk.Tk()
+app.geometry('600x300')
+app.title('Treadmil Users Analysis')
+
+## Radio Button
+
+
+graphs = ttk.Variable(app)
+values = {'Pair Plot':'sns.pairplot(data = data)',
+ 'Jointplot':"sns.jointplot(data = data, x='{col1}',y='{col2}')",
+ 'Bar Plot': "sns.barplot(data =data, x = '{col}',y='{col2}')"}
+graphs.set(values['Pair Plot'])
+y = 10
+
+for key,value in values.items():
+    ttk.Radiobutton(app, text = key, variable=graphs, value = value).place(x = 10,y = y)
+    y +=20
+
+
+## Option Menu 1
+col1 = ttk.Variable(app)
+values =['Select'] + list(data.columns)
+col1.set(values[0])
+ttk.Label(app, text = 'Column 1').place(x = 150,y = 10)
+ttk.OptionMenu(app, col1,*values).place(x = 150,y = 40)
+
+## Option Menu 2
+col2 = ttk.Variable(app)
+col2.set(values[0])
+ttk.Label(app, text = 'Column 2').place(x = 100,y = 10)
+ttk.OptionMenu(app, col2,*values).place(x = 100,y = 10)
+
+## Option Menu 3
+col3 = ttk.Variable(app)
+col3.set(values[0])
+ttk.Label(app, text = 'Column 3').place(x = 150,y = 150)
+ttk.OptionMenu(app, col3,*values).place(x = 150,y = 180)
+
+def show():
+    # print(graphs.get())
+    print(col1.get(),col2.get(),col3.get())
+
+ttk.Button(app, text='Show', command = show).place(x=400,y=10)
+
+app.mainloop()
